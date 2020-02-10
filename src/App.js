8c4e0313ea2ace257a2910a7b6f56afb3a089327
Guillaume Buaud <GuiGui@iMac-de-Louise.local>
2020-02-10 21:31:54 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { title: "faire les courses", isDone: false },
+    { title: "Arroser le splantes", isDone: false },
+    { title: "Aller au musée", isDone: true },
+    { title: "Nettoyer la voiture", isDone: true }
+  ]);
+  const [newTaskInput, setNewTaskInput] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ToDo List</h1>
+
+      {tasks.map((task, index) => {
+        return (
+          <div>
+            <input
+              checked={task.isDone}
+              type="checkbox"
+              onClick={() => {
+                const newTasks = [...tasks];
+                newTasks[index].isDone = !newTasks[index].isDone;
+                setTasks(newTasks);
+              }}
+            />
+            <span className={task.isDone === true ? "checked" : ""}>
+              {task.title}
+            </span>
+            <button
+              onClick={() => {
+                const newTasks = [...tasks];
+                newTasks.splice(index, 1);
+                setTasks(newTasks);
+              }}
+            >
+              Supprimer
+            </button>
+          </div>
+        );
+      })}
+
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          const newTasks = [...tasks];
+          newTasks.push({
+            title: newTaskInput,
+            isDone: false
+          });
+          setTasks(newTasks);
+          setNewTaskInput("");
+        }}
+      >
+        <input
+          type="text"
+          value={newTaskInput}
+          onChange={event => {
+            const value = event.target.value;
+            setNewTaskInput(value);
+          }}
+        />
+        <button type="submit">Add task</button>
+      </form>
     </div>
   );
 }
